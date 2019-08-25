fb08eaf82eee2b5ec57c1283265b800bc9269882
Yuki Okada <y.okady.221@gmail.com>
2019-08-25 23:37:07 +0900

    数値を抽出

--- lib/main.dart
+++ lib/main.dart
@@ -34,21 +34,79 @@ class _MyHomePageState extends State<MyHomePage> {
     return await ImagePicker.pickImage(source: ImageSource.gallery);
   }
 
+  double _findNumeric(String text, List<String> texts, int index) {
+    int prevIndex = index - 1;
+    if (text.isEmpty) {
+      text = texts[prevIndex];
+      prevIndex -= 1;
+    }
+    if (!text.contains(".") || text.startsWith(".")) {
+      text = texts[prevIndex] + text;
+      prevIndex -= 1;
+    }
+    return double.tryParse(text) ?? 0.0;
+  }
+
   Future<void> _scanImage(final File image) async {
     final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
     final TextRecognizer textRecognizer = FirebaseVision.instance.cloudTextRecognizer();
     final VisionText visionText = await textRecognizer.processImage(visionImage);
-    for (TextBlock block in visionText.blocks) {
-      String text = "";
-      for (TextLine line in block.lines) {
-        for (TextElement element in line.elements) {
-          text += element.text + " ";
-        }
-        text += "\n";
-      }
-      print(text);
-    }
+    List<String> texts = [];
+    visionText.blocks.forEach((TextBlock block) {
+      block.lines.forEach((TextLine line) {
+        texts.addAll(line.elements.map((element) => element.text.replaceAll(",", ".").replaceAll("O", "0")));
+      });
+    });
     textRecognizer.close();
+    String date;
+    List<double> weights = [];
+    List<double> percentages = [];
+    double bmi;
+    for (int i = 0; i < texts.length; i++) {
+      if ((new RegExp("^[0-9]{4}\.[0-9]{1,2}\.[0-9]{1,2}\$").hasMatch(texts[i]))) {
+        if (date == null) {
+          date = texts[i];
+        }
+      } else if (texts[i].endsWith("kg")) {
+        double value = _findNumeric(texts[i].replaceAll("kg", ""), texts, i);
+        if (value > 0.0) {
+          weights.add(value);
+        }
+      } else if (texts[i].endsWith("%")) {
+        double value = _findNumeric(texts[i].replaceAll("%", ""), texts, i);
+        if (value > 0.0) {
+          percentages.add(value);
+        }
+      } else if (texts[i].contains("kg/m")) {
+        bmi = _findNumeric(texts[i].replaceAll("kg/m2", "").replaceAll("kg/m", ""), texts, i);
+      }
+    }
+    // ヘッダーの体重
+    weights.removeAt(0);
+    // エクササイズプランの体重
+    if (weights[3] == weights[0]) {
+      weights.removeAt(3);
+    } else if (weights[4] == weights[0]) {
+      weights.removeAt(4);
+    }
+
+    // texts.forEach((text) => print(text));
+    print("日付: " + date);
+    print("体重: " + weights[0].toString() + " kg");
+    print("筋肉: " + weights[1].toString() + " kg");
+    print("体脂肪: " + weights[2].toString() + " kg");
+    print("右腕: " + weights[3].toString() + " kg");
+    print("左腕: " + weights[4].toString() + " kg");
+    print("胴体: " + weights[5].toString() + " kg");
+    print("右脚: " + weights[6].toString() + " kg");
+    print("左脚: " + weights[7].toString() + " kg");
+    print("BMI: " + bmi.toString() + " kg/m2");
+    print("体脂肪率: " + percentages[0].toString() + " %");
+    print("発達程度（胴体）: " + percentages[1].toString() + " %");
+    print("発達程度（右脚）: " + percentages[2].toString() + " %");
+    print("発達程度（左脚）: " + percentages[3].toString() + " %");
+    // print(weights.join(", "));
+    // print(percentages.join(", "));
   }
 
   Future<void> _loadImage() async {
