5646be4a97ec7935d27eee1ad9e6c96ba2831034
Yuki Okada <y.okady.221@gmail.com>
2019-08-26 20:42:37 +0900

    データ登録フォーム

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class InBodyData {
+  String date;
+  double bodyWeight;
+  double muscleWeight;
+  double bodyFatWeight;
+  double rightArmWeight;
+  double leftArmWeight;
+  double trunkWeight;
+  double rightLegWeight;
+  double leftLegWeight;
+  double bmi;
+  double bodyFatPercentage;
+  double trunkPercentage;
+  double rightLegPercentage;
+  double leftLegPercentage;
+
+  void dump() {
+    print("日付: " + date);
+    print("体重: " + bodyWeight.toString() + " kg");
+    print("筋肉: " + muscleWeight.toString() + " kg");
+    print("体脂肪: " + bodyFatWeight.toString() + " kg");
+    print("右腕: " + rightArmWeight.toString() + " kg");
+    print("左腕: " + leftArmWeight.toString() + " kg");
+    print("胴体: " + trunkWeight.toString() + " kg");
+    print("右脚: " + rightLegWeight.toString() + " kg");
+    print("左脚: " + leftLegWeight.toString() + " kg");
+    print("BMI: " + bmi.toString() + " kg/m2");
+    print("体脂肪率: " + bodyFatPercentage.toString() + " %");
+    print("発達程度（胴体）: " + trunkPercentage.toString() + " %");
+    print("発達程度（右脚）: " + rightLegPercentage.toString() + " %");
+    print("発達程度（左脚）: " + leftLegPercentage.toString() + " %");
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -28,8 +62,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  File _image;
-
   Future<File> _pickImage() async {
     return await ImagePicker.pickImage(source: ImageSource.gallery);
   }
@@ -40,45 +72,45 @@ class _MyHomePageState extends State<MyHomePage> {
       text = texts[prevIndex];
       prevIndex -= 1;
     }
-    if (!text.contains(".") || text.startsWith(".")) {
+    if (!text.contains('.') || text.startsWith('.')) {
       text = texts[prevIndex] + text;
       prevIndex -= 1;
     }
     return double.tryParse(text) ?? 0.0;
   }
 
-  Future<void> _scanImage(final File image) async {
+  Future<InBodyData> _scanImage(final File image) async {
     final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
     final TextRecognizer textRecognizer = FirebaseVision.instance.cloudTextRecognizer();
     final VisionText visionText = await textRecognizer.processImage(visionImage);
     List<String> texts = [];
     visionText.blocks.forEach((TextBlock block) {
       block.lines.forEach((TextLine line) {
-        texts.addAll(line.elements.map((element) => element.text.replaceAll(",", ".").replaceAll("O", "0")));
+        texts.addAll(line.elements.map((element) => element.text.replaceAll(',', '.').replaceAll('O', '0')));
       });
     });
     textRecognizer.close();
-    String date;
+
+    InBodyData data = InBodyData();
     List<double> weights = [];
     List<double> percentages = [];
-    double bmi;
     for (int i = 0; i < texts.length; i++) {
-      if ((new RegExp("^[0-9]{4}\.[0-9]{1,2}\.[0-9]{1,2}\$").hasMatch(texts[i]))) {
-        if (date == null) {
-          date = texts[i];
+      if ((new RegExp('^[0-9]{4}\.[0-9]{1,2}\.[0-9]{1,2}\$').hasMatch(texts[i]))) {
+        if (data.date == null) {
+          data.date = texts[i];
         }
-      } else if (texts[i].endsWith("kg")) {
-        double value = _findNumeric(texts[i].replaceAll("kg", ""), texts, i);
+      } else if (texts[i].endsWith('kg')) {
+        double value = _findNumeric(texts[i].replaceAll('kg', ''), texts, i);
         if (value > 0.0) {
           weights.add(value);
         }
-      } else if (texts[i].endsWith("%")) {
-        double value = _findNumeric(texts[i].replaceAll("%", ""), texts, i);
+      } else if (texts[i].endsWith('%')) {
+        double value = _findNumeric(texts[i].replaceAll('%', ''), texts, i);
         if (value > 0.0) {
           percentages.add(value);
         }
-      } else if (texts[i].contains("kg/m")) {
-        bmi = _findNumeric(texts[i].replaceAll("kg/m2", "").replaceAll("kg/m", ""), texts, i);
+      } else if (texts[i].contains('kg/m')) {
+        data.bmi = _findNumeric(texts[i].replaceAll('kg/m2', '').replaceAll('kg/m', ''), texts, i);
       }
     }
     // ヘッダーの体重
@@ -90,23 +122,21 @@ class _MyHomePageState extends State<MyHomePage> {
       weights.removeAt(4);
     }
 
-    // texts.forEach((text) => print(text));
-    print("日付: " + date);
-    print("体重: " + weights[0].toString() + " kg");
-    print("筋肉: " + weights[1].toString() + " kg");
-    print("体脂肪: " + weights[2].toString() + " kg");
-    print("右腕: " + weights[3].toString() + " kg");
-    print("左腕: " + weights[4].toString() + " kg");
-    print("胴体: " + weights[5].toString() + " kg");
-    print("右脚: " + weights[6].toString() + " kg");
-    print("左脚: " + weights[7].toString() + " kg");
-    print("BMI: " + bmi.toString() + " kg/m2");
-    print("体脂肪率: " + percentages[0].toString() + " %");
-    print("発達程度（胴体）: " + percentages[1].toString() + " %");
-    print("発達程度（右脚）: " + percentages[2].toString() + " %");
-    print("発達程度（左脚）: " + percentages[3].toString() + " %");
-    // print(weights.join(", "));
-    // print(percentages.join(", "));
+    data.bodyWeight = weights[0];
+    data.muscleWeight = weights[1];
+    data.bodyFatWeight = weights[2];
+    data.rightArmWeight = weights[3];
+    data.leftArmWeight = weights[4];
+    data.trunkWeight = weights[5];
+    data.rightLegWeight = weights[6];
+    data.leftLegWeight = weights[7];
+    data.bodyFatPercentage = percentages[0];
+    data.trunkPercentage = percentages[1];
+    data.rightLegPercentage = percentages[2];
+    data.leftLegPercentage = percentages[3];
+    // data.dump();
+
+    return data;
   }
 
   Future<void> _loadImage() async {
@@ -114,10 +144,16 @@ class _MyHomePageState extends State<MyHomePage> {
     if (image == null) {
       return;
     }
-    setState(() {
-      _image = image;
-    });
-    _scanImage(image);
+    InBodyData data = await _scanImage(image);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return InBodyForm(data: data);
+        },
+        fullscreenDialog: true,
+      )
+    );
   }
 
   @override
@@ -128,18 +164,18 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            _image != null ? Container(
-              height: 200.0,
-              width: 200.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: FileImage(_image),
-                  fit: BoxFit.cover,
-                )
-              ),
-            ) : Container()
+            // _image != null ? Container(
+            //   height: 200.0,
+            //   width: 200.0,
+            //   decoration: BoxDecoration(
+            //     image: DecorationImage(
+            //       image: FileImage(_image),
+            //       fit: BoxFit.cover,
+            //     )
+            //   ),
+            // ) : Container(),
           ],
         ),
       ),
@@ -147,6 +183,192 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _loadImage,
         tooltip: '画像を読み込む',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class InBodyForm extends StatefulWidget {
+  InBodyForm({Key key, this.data}) : super(key: key);
+
+  final InBodyData data;
+
+  @override
+  _InBodyFormState createState() => _InBodyFormState();
+}
+
+class _InBodyFormState extends State<InBodyForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _dateController;
+  TextEditingController _bodyWeightController;
+  TextEditingController _muscleWeightController;
+  TextEditingController _bodyFatWeightController;
+  TextEditingController _rightArmWeightController;
+  TextEditingController _leftArmWeightController;
+  TextEditingController _trunkWeightController;
+  TextEditingController _rightLegWeightController;
+  TextEditingController _leftLegWeightController;
+  TextEditingController _bmiController;
+  TextEditingController _bodyFatPercentageController;
+  TextEditingController _trunkPercentageController;
+  TextEditingController _rightLegPercentageController;
+  TextEditingController _leftLegPercentageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _dateController = TextEditingController(text: widget.data.date);
+    _bodyWeightController = TextEditingController(text: widget.data.bodyWeight.toString());
+    _muscleWeightController = TextEditingController(text: widget.data.muscleWeight.toString());
+    _bodyFatWeightController = TextEditingController(text: widget.data.bodyFatWeight.toString());
+    _rightArmWeightController = TextEditingController(text: widget.data.rightArmWeight.toString());
+    _leftArmWeightController = TextEditingController(text: widget.data.leftArmWeight.toString());
+    _trunkWeightController = TextEditingController(text: widget.data.trunkWeight.toString());
+    _rightLegWeightController = TextEditingController(text: widget.data.rightLegWeight.toString());
+    _leftLegWeightController = TextEditingController(text: widget.data.leftLegWeight.toString());
+    _bmiController = TextEditingController(text: widget.data.bmi.toString());
+    _bodyFatPercentageController = TextEditingController(text: widget.data.bodyFatPercentage.toString());
+    _trunkPercentageController = TextEditingController(text: widget.data.trunkPercentage.toString());
+    _rightLegPercentageController = TextEditingController(text: widget.data.rightLegPercentage.toString());
+    _leftLegPercentageController = TextEditingController(text: widget.data.leftLegPercentage.toString());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _dateController.dispose();
+    _bodyWeightController.dispose();
+    _muscleWeightController.dispose();
+    _bodyFatWeightController.dispose();
+    _rightArmWeightController.dispose();
+    _leftArmWeightController.dispose();
+    _trunkWeightController.dispose();
+    _rightLegWeightController.dispose();
+    _leftLegWeightController.dispose();
+    _bmiController.dispose();
+    _bodyFatPercentageController.dispose();
+    _trunkPercentageController.dispose();
+    _rightLegPercentageController.dispose();
+    _leftLegPercentageController.dispose();
+  }
+
+  _save() {
+    if (_formKey.currentState.validate()) {
+      InBodyData data = InBodyData();
+      data.date = _dateController.text;
+      data.bodyWeight = double.parse(_bodyWeightController.text);
+      data.muscleWeight = double.parse(_muscleWeightController.text);
+      data.bodyFatWeight = double.parse(_bodyFatWeightController.text);
+      data.rightArmWeight = double.parse(_rightArmWeightController.text);
+      data.leftArmWeight = double.parse(_leftArmWeightController.text);
+      data.trunkWeight = double.parse(_trunkWeightController.text);
+      data.rightLegWeight = double.parse(_rightLegWeightController.text);
+      data.leftLegWeight = double.parse(_leftLegWeightController.text);
+      data.bmi = double.parse(_bmiController.text);
+      data.bodyFatPercentage = double.parse(_bodyFatPercentageController.text);
+      data.trunkPercentage = double.parse(_trunkPercentageController.text);
+      data.rightLegPercentage = double.parse(_rightLegPercentageController.text);
+      data.leftLegPercentage = double.parse(_leftLegPercentageController.text);
+      // data.dump();
+      Navigator.pop(context);
+    } else {
+      // TODO: error handling
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("登録"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(8.0),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: new Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '日付'),
+                    keyboardType: TextInputType.text,
+                    controller: _dateController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '体重 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _bodyWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '筋肉 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _muscleWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '体脂肪 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _bodyFatWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '右腕 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _rightArmWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '左腕 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _leftArmWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '胴体 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _trunkWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '右脚 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _rightLegWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '左脚 (kg)'),
+                    keyboardType: TextInputType.text,
+                    controller: _leftLegWeightController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'BMI (kg/m2)'),
+                    keyboardType: TextInputType.text,
+                    controller: _bmiController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '体脂肪率 (%)'),
+                    keyboardType: TextInputType.text,
+                    controller: _bodyFatPercentageController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '発達程度:胴体 (%)'),
+                    keyboardType: TextInputType.text,
+                    controller: _trunkPercentageController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '発達程度:右脚 (%)'),
+                    keyboardType: TextInputType.text,
+                    controller: _rightLegPercentageController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: '発達程度:左脚 (%)'),
+                    keyboardType: TextInputType.text,
+                    controller: _leftLegPercentageController,
+                  ),
+                  RaisedButton(
+                    onPressed: _save,
+                    child: Text("登録する"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
