e8d0be3875d259ba68846dd0713205c3b70a6d02
Yuki Okada <y.okady.221@gmail.com>
2022-04-05 22:20:23 +0900

    #25 dotenvの使い方を修正

--- lib/Env.dart
+++ lib/Env.dart
@@ -4,12 +4,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Env {
   static const APP_NAME = 'InBody Scanner';
 
-  static Future<dynamic> load() =>
-    DotEnv().load(fileName: '.env');
+  static Future<dynamic> load() => dotenv.load(fileName: '.env');
 
   static String getAdMobAppId() =>
-    DotEnv().env['ADMOB_APP_ID_${Platform.isIOS ? "IOS" : "ANDROID"}'];
+      dotenv.env['ADMOB_APP_ID_${Platform.isIOS ? "IOS" : "ANDROID"}'];
 
   static String getAdMobUnitId() =>
-    DotEnv().env['ADMOB_UNIT_ID_${Platform.isIOS ? "IOS" : "ANDROID"}'];
+      dotenv.env['ADMOB_UNIT_ID_${Platform.isIOS ? "IOS" : "ANDROID"}'];
 }
